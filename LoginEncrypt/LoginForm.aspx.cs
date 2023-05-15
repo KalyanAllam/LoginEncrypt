97f5953aebc8b5187c66fe89994d621eaaa2c618
KALYAN <allam.kalyana@outlook.com>
2023-05-15 16:34:20 +0530

    Add project files.

---
+++ LoginEncrypt/LoginForm.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Npgsql;
+
+namespace LoginEncrypt
+{
+    public partial class LoginForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Session["Username"] = txtEmail.Text;
+            }
+        }
+        private string Decrypt(string clearText)
+        {
+            string EncryptionKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            var con = new NpgsqlConnection(CS);
+          
+               
+                con.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT COUNT(*) FROM userregistration where email=@Email and password=@Password ", con);
+
+              cmd.Parameters.AddWithValue("Email", txtEmail.Text.Trim());
+                cmd.Parameters.AddWithValue("Password", Decrypt(txtPassword.Text.Trim()));
+            Int64 Username = (Int64)cmd.ExecuteScalar();
+
+            if (Username == 1)
+                {
+                    Session["Username"] = txtEmail.Text;
+                    Response.Redirect("Welcome.aspx");
+                    Session.RemoveAll();
+
+                }
+                else
+                {
+                    lblMessage.ForeColor = System.Drawing.Color.Red;
+                    lblMessage.Text = "Invalid username and password";
+                }
+          
+
+
+            
+
+
+
+        }
+    }
+}
